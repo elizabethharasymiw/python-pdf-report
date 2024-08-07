c4a4513e0b6c5ff0d18333a7fd893f33409b3f5d
elizabethharasymiw <elizabeth.harasymiw@gmail.com>
2024-08-06 21:47:45 -0500

    Increase font size to 18

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ pdf = FPDF()
 pdf.add_page()
 
 # Set font
-pdf.set_font("Arial", size=12)
+pdf.set_font("Arial", size=18)
 
 # Add a cell
 pdf.cell(200, 10, txt="Welcome to FPDF!", ln=True, align='C')
