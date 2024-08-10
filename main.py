5905a379a543adb66e7181071d9a85c453b280e1
elizabethharasymiw <elizabeth.harasymiw@gmail.com>
2024-08-09 21:39:32 -0500

    Add test image to example pdf

--- main.py
+++ main.py
@@ -19,5 +19,8 @@ pdf.set_font("Arial", size=12)
 pdf.cell(200, 10, txt="Text Line 1", ln=True, align='C')
 pdf.cell(200, 10, txt="Text Line 2", ln=True, align='C')
 
+# Add an image
+pdf.image("./TestXImage.png", x=10, y=30, w=50)
+
 # Save the PDF with name .pdf
 pdf.output("example.pdf")
