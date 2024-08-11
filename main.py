f0ef710f9560c5677b3187ba6356f860aa2d6541
elizabethharasymiw <elizabeth.harasymiw@gmail.com>
2024-08-10 23:16:24 -0500

    Add bullet point list example

--- main.py
+++ main.py
@@ -22,5 +22,20 @@ pdf.cell(200, 10, txt="Text Line 2", ln=True, align='C')
 # Add an image
 pdf.image("./TestXImage.png", x=10, y=30, w=50)
 
+# Move the cursor to a new line to add bullet points
+pdf.ln(60)
+
+# Bullet points
+bullet_points = [
+    "Bullet point 1",
+    "Bullet point 2",
+    "Bullet point 3"
+]
+
+# Add bullet points to the PDF
+for point in bullet_points:
+    pdf.cell(10, 10, txt="-", ln=False)
+    pdf.cell(190, 10, txt=point, ln=True)
+
 # Save the PDF with name .pdf
 pdf.output("example.pdf")
