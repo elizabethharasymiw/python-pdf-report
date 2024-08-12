ef18fefad23e9f0e1cf825fe217fcf8ff90a96a2
elizabethharasymiw <elizabeth.harasymiw@gmail.com>
2024-08-11 22:09:02 -0500

    Bold the title text example

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ pdf = FPDF()
 pdf.add_page()
 
 # Set Title font
-pdf.set_font("Arial", size=32)
+pdf.set_font("Arial", style='B', size=32)
 
 # Add a cell
 pdf.cell(200, 10, txt="Welcome to FPDF!", ln=True, align='C')
